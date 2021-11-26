6f7a780de69bf36e2904441ec94a9e8b6e8ea85d
dante <maxgamerdu@gmail.com>
2021-11-26 15:53:08 +0300

    added a section that allows you to place your order

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
-function keep(nameshit,mailshit) {this.name=nameshit; this.mail=mailshit}
-$(document).ready(function () {
-    $('form#clear').submit(function (e) { 
-        e.preventDefault();
-
-        var first = $("input#first").val();
-        var second = $("input#mail").val();
-
-       console.log(first);
-       console.log(second);
-        
-    });
-});
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+    
+        onSliderLoad: function() {
+            $('.autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+  
