6eb9eccaf46d56337980ae5e268486123b393d95
dante <maxgamerdu@gmail.com>
2021-11-26 16:47:13 +0300

    added business logic to my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-    
-        onSliderLoad: function() {
-            $('.autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-  });
+function Pizza(pizza, size, crust, topping,amount) {
+    this.pizza = pizza;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.amount = amount;
+  
+  }
+  
+  var priceSize, priceCrust, priceTopping,pizzaAmount;
+  var displayOrdersModal = new bootstrap.Modal(document.getElementById('displayOrdersModal'))
+  var checkoutModal = new bootstrap.Modal(document.getElementById('checkoutModal'));
+  
   
