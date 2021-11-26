692d4bb3e7669eb8fea913f8015d758c71af8f25
dante <maxgamerdu@gmail.com>
2021-11-26 17:09:10 +0300

    added a section to initiate the calculation of values dependant on the pizza selected values

--- js/scripts.js
+++ js/scripts.js
@@ -38,3 +38,54 @@ Pizza.prototype.getToppingPrice = function () {
       return 0;
     }
   };
+  // pizza size time
+  Pizza.prototype.getPizzaSizePrice = function () {
+    switch (this.size) {
+    
+      case "large":
+        return 1200;
+      case "medium":
+        return 1000;
+      case "small":
+        return 800;
+      default:
+                
+          return null;
+  }
+  };
+  //pizza total
+  Pizza.prototype.getPizzaPrice = function () {
+    return (this.getCrustPrice() + this.getToppingPrice() + this.getPizzaSizePrice())* this.amount;
+ };
+ 
+ 
+ Pizza.prototype.getPizzaPrice = function () {
+    return (this.getCrustPrice() + this.getToppingPrice() + this.getPizzaSizePrice())* this.amount;
+ };
+ 
+ 
+ var resetForm = () =>{
+   $("#pizzaCategory ").val('');
+   $("#pizzaOption ").val('...');
+   $("#pizzaSize ").val('');
+   $("#crust ").val('');
+   $("#topping ").val('');
+   $("#amount").val( '');
+ };
+ $(document).ready(function() {
+      $(function($) {
+        var Options = {
+            'meatarian': ['...','Pesto','Artichoke','Steakhouse pizza','Club pizza','chicken pizza' , 'Buffalo beef pizza','Chilli pizza','Meat lovers pizza'],
+            'vegetarian': ['...','spinach Pizza','Grilled Tomato Pizza',' Mushroom Pizza','Garlic Pizza','Mushroom Pizza','Pepper Pizza','Kale pizza'],
+              };
+    
+        var $Options = $('#pizzaOption');
+        $('#pizzaCategory').change(function () {
+            var pizzaCategory = $(this).val(), pizzas = Options[pizzaCategory] || [];
+    
+            var html = $.map(pizzas, function(pizza){
+                return '<option value="' + pizza + '">' + pizza + '</option>';
+            }).join('');
+            $Options.html(html);
+        });
+    });
