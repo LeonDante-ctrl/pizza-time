4e524dda865b89f59a7337843008d52a9969d46d
dante <maxgamerdu@gmail.com>
2021-12-01 09:46:47 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# NAME
+Dante's Pizzeria
+
+## AUTHOR
+Leon Dante
+
+### Description
+Dante's pizzeria is a web application for an outlet that offers the best pizza in Nairobi. This application enables the outlet sell pizzas online to clients through online orders. The clients can be able to select their favorite pizza according to the topping  They can also select the crust or base of the pizza whether crispy, Stuffed or Gluten-free. Finally, they will be able to select the size of the pizza, whether small, medium or large. and from there on out , we make our delivery
+
+
+### Technologies used and installation set-up
+The program has been made using VScode for coding, Git for tracking commits, Github for remote storage and any web-browser can be used for access by the clients who want to use it, but the author recommends Google Chrome.
+
+### Link to live Github pages
+https://LeonDante-ctrl.github.io/pizza-time
+
+### License and Copyright Infomation
+The author has given permission to anyone who would like to use this app absolute right. However, update should have written authority from Dante's inc 2021 (c).
+
+The author grants permission free of charge, to any person obtaining a copy of this program code and files to deal
+in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sub-license, and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+to contribute to or update for better user experience or third party use subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANT-ABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN C
--- js/scripts.js
+++ js/scripts.js
@@ -1,91 +1,241 @@
 function Pizza(pizza, size, crust, topping,amount) {
-    this.pizza = pizza;
-    this.size = size;
-    this.crust = crust;
-    this.topping = topping;
-    this.amount = amount;
-  
-  }
-  
-  var priceSize, priceCrust, priceTopping,pizzaAmount;
-  var displayOrdersModal = new bootstrap.Modal(document.getElementById('displayOrdersModal'));
-  var checkoutModal = new bootstrap.Modal(document.getElementById('checkoutModal'));
+  this.pizza = pizza;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.amount = amount;
 
-  
-// sourcing the topping price section
+}
+
+var priceSize, priceCrust, priceTopping,pizzaAmount;
+var displayOrdersModal = new bootstrap.Modal(document.getElementById('displayOrdersModal'));
+var checkoutModal = new bootstrap.Modal(document.getElementById('checkoutModal'));
+
+
+//Get topping price
 Pizza.prototype.getToppingPrice = function () {
-    switch (this.size) {
-        case "large":
-        return 200;
-      case "medium":
-        return 150;
-      case "small":
-        return 100;
-      default:
-                        return null;
-  }
-  };
-  // crust section
-  Pizza.prototype.getCrustPrice = function () {
-    if (this.crust === "crispy") {
-      return 100;
-    } else if (this.crust === "stuffed") {
-      return 200;
-    } else if (this.crust === "glutten-free") {
-      return 150;
-    }
-    else {
-      return 0;
-    }
-  };
-  // pizza size time
-  Pizza.prototype.getPizzaSizePrice = function () {
-    switch (this.size) {
-    
+  switch (this.size) {
       case "large":
-        return 1200;
-      case "medium":
-        return 1000;
-      case "small":
-        return 800;
-      default:
-                
-          return null;
+      return 200;
+        break;
+    case "medium":
+      return 150;
+        break;
+    case "small":
+      return 100;
+        break;
+    default:
+                      return null;
+};
+};
+
+//Get Crust price
+Pizza.prototype.getCrustPrice = function () {
+  if (this.crust === "crispy") {
+    return 100;
+  } else if (this.crust === "stuffed") {
+    return 200;
+  } else if (this.crust === "glutten-free") {
+    return 150;
   }
-  };
-  //pizza total
-  Pizza.prototype.getPizzaPrice = function () {
-    return (this.getCrustPrice() + this.getToppingPrice() + this.getPizzaSizePrice())* this.amount;
- };
- 
- 
- Pizza.prototype.getPizzaPrice = function () {
-    return (this.getCrustPrice() + this.getToppingPrice() + this.getPizzaSizePrice())* this.amount;
- };
- 
- 
- var resetForm = () =>{
-   $("#pizzaCategory ").val('');
-   $("#pizzaOption ").val('...');
-   $("#pizzaSize ").val('');
-   $("#crust ").val('');
-   $("#topping ").val('');
-   $("#amount").val( '');
- };
- $(document).ready(function() {
-      $(function($) {
-        var Options = {
-            'meatarian': ['...','Pesto','Artichoke','Steakhouse pizza','Club pizza','chicken pizza' , 'Buffalo beef pizza','Chilli pizza','Meat lovers pizza'],
-            'vegetarian': ['...','spinach Pizza','Grilled Tomato Pizza',' Mushroom Pizza','Garlic Pizza','Mushroom Pizza','Pepper Pizza','Kale pizza'],
-              };
-    
-        var $Options = $('#pizzaOption');
-        $('#pizzaCategory').change(function () {
-            var pizzaCategory = $(this).val(), pizzas = Options[pizzaCategory] || [];
-    
-            var html = $.map(pizzas, function(pizza){
-                return '<option value="' + pizza + '">' + pizza + '</option>';
-            }).join('');
-            $Options.html(html);
-        });
+  else {
+    return 0;
+  }
+};
+
+//Get pizza size price
+Pizza.prototype.getPizzaSizePrice = function () {
+  switch (this.size) {
+  
+    case "large":
+      return 1200;
+        break;
+    case "medium":
+      return 1000;
+        break;
+    case "small":
+      return 800;
+        break;
+    default:
+              
+        return null;
+};
+};
+
+//Get total pizza price
+Pizza.prototype.getPizzaPrice = function () {
+   return (this.getCrustPrice() + this.getToppingPrice() + this.getPizzaSizePrice())* this.amount;
+};
+
+
+let resetForm = (=>{
+  $("#pizzaCategory ").val('');
+  $("#pizzaOption ").val('...');
+  $("#pizzaSize ").val('');
+  $("#crust ").val('');
+  $("#topping ").val('');
+  $("#amount").val( '');
+});
+
+
+
+
+$(document).ready(function() {
+// seting values by result
+  $(function($) {
+    var Options = {
+      'meatarian': ['...','Pesto','Artichoke','Steakhouse pizza','Club pizza','chicken pizza' , 'Buffalo beef pizza','Chilli pizza','Meat lovers pizza'],
+      'vegetarian': ['...','spinach Pizza','Grilled Tomato Pizza',' Mushroom Pizza','Garlic Pizza','Mushroom Pizza','Pepper Pizza','Kale pizza'],
+          };
+
+    var $Options = $('#pizzaOption');
+    $('#pizzaCategory').change(function () {
+        var pizzaCategory = $(this).val(), pizzas = Options[pizzaCategory] || [];
+
+        var html = $.map(pizzas, function(pizza){
+            return '<option value="' + pizza + '">' + pizza + '</option>'
+        }).join('');
+        $Options.html(html);
     });
+});
+
+let pizzaOrders = [];
+let pizzaOrderPrices = [];
+
+$("form#deliveryForm").submit(function(event) {
+  event.preventDefault();
+  let pizzaName = $("#pizzaOption option:selected").val();
+  let pizzaSize = $("#pizzaSize option:selected").val();
+  let pizzaCrust = $("#crust option:selected").val();
+  let pizzaTopping = $("#topping option:selected").val();
+  let pizzaAmount = parseInt($("#amount").val());
+
+if(pizzaAmount < 1){
+  alert('Please select amount of pizza that you want to order');
+  return;
+}
+
+newPizzaOrder = new Pizza(pizzaName,pizzaSize,pizzaCrust,pizzaTopping,pizzaAmount)
+pizzaOrders.push(newPizzaOrder)
+pizzaOrderPrices.push(newPizzaOrder.getPizzaPrice())
+
+
+resetForm();
+
+
+if(pizzaOrders.length >= 1 ){
+  $('.cart').fadeIn()
+  $('#cart-count').text(`${pizzaOrders.length}`)
+  $('#card').click(()=>{
+    newPizzaOrder.getPizzaPrice()
+  })
+}
+
+var resetOrders = ()=>{
+pizzaOrders = []
+  $("#order-summary").empty()
+}
+
+
+
+$("#order-summary").append(
+  "<tr>" +
+    '<th scope="row">' +
+      newPizzaOrder.pizza +
+    " (" +
+      newPizzaOrder.size +
+    ") - Ksh." +
+  newPizzaOrder.getPizzaSizePrice() +
+    "</th>" +
+    "<td>" +
+    newPizzaOrder.topping +
+    " - Ksh." +
+  newPizzaOrder.getToppingPrice() +
+    "</td>" +
+    "<td>" +
+      newPizzaOrder.crust +
+    " - Ksh." +
+  newPizzaOrder.getCrustPrice() +
+    "</td>" +
+    "<td>" +
+  newPizzaOrder.amount +
+    "</td>" +
+    "<td>" + 'Ksh.'+
+    newPizzaOrder.getPizzaPrice() +
+        "</td>" +
+    "</tr>"
+);
+
+
+
+totalCost = pizzaOrderPrices.reduce((a,b)=>a+b,0);
+
+let costWithDelivery = totalCost + 300
+
+
+$("#total-amount").text(totalCost);
+
+
+
+var collect = true;
+
+$("form#deliveryForm").submit(function(event) {
+  resetOrders()
+  event.preventDefault();
+  collect = false;
+var customerName = $('#fullName').val();
+var customerLocation = $('#location').val()
+
+if(!collect){
+  $('#checkoutText').text(`  Dear ${customerName} your order will be delivered to ${customerLocation} within
+  two hours! Your order total is Ksh.${costWithDelivery} you can reach us at 0721399458`)
+
+$('.cart').fadeOut()
+setTimeout(function(){location.reload();}, 6000);
+}
+
+});
+
+
+$('#modalBtn').click(()=>{
+displayOrdersModal.show()
+})
+
+$('#deliveryBtn').click(()=>{
+  $('#collectOption > button').fadeOut(200)
+  $('.delivery-form > form').fadeIn(500)
+   })
+
+
+$('.checkoutBtn').click(()=>{
+resetOrders()
+displayOrdersModal.hide()
+  checkoutModal.show()
+  if(collect){
+    $('#checkoutText').text(`Dear Customer your order will be ready within the hour! Your order total is Ksh.${totalCost}.For any inquries you can reach us at 0721000000`);
+  
+     $('.cart').fadeOut()
+
+     setTimeout(function(){location.reload(); }, 6000);
+  }
+})
+
+
+});
+
+
+
+//Code for jquery lightSlider
+$(document).ready(function() {
+  $('.autoWidth').lightSlider({
+      autoWidth:true,
+      // loop:true,
+      onSliderLoad: function() {
+          $('.autoWidth').removeClass('cS-hidden');
+      } 
+  });  
+});
+
+
+
+})
